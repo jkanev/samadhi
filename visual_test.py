f63a3bdda4144233e14fb69e421a5eb6c83103e9
Jacob Kanev <jkanev@zoho.com>
2024-10-17 23:05:10 +0200

    Added more fancy things to visual test
    Added rotations to the sine waves (using np.roll()), made the
    number of points per circle variable, plotted with dots instead
    of lines, added changing colours. Added fullscreen option and
    improved figure stretch.

--- visual_test.py
+++ visual_test.py
@@ -14,14 +14,15 @@ f = [(abs(np.sin(1*t0))),
      ((np.sin(2*t0)+1)*10.0)]
 
 # Create 10 circles of different lengths
-M = 40
+M = 40 # number of circles
+N = 200 # points per circle
 lines = [None] * M
-ylim = 40
-thickness = 2.0
+ylim = 32
+thickness = 0.5
 ax = None
 fig = None
 freq_start = 1.0
-freq_step = 0.01
+freq_step = 0.005
 r = [[]] * M  # the interpolated circle data, zeros for now, consisting of M*N circles s[M][1..5], all the same length
 s = [[]] * M  # the raw circle data, zeros for now, consisting of M*N circles s[M][1..5]
 t = [[]] * M  # the base to plot against, will go from 0 to 2pi, t[M]
@@ -32,7 +33,7 @@ for m in range(0, M):
     for n in range(0, 5):
         s[m][n] = np.zeros(int((2 / freq) * np.pi / dt))
     #t[m] = [m for m in np.arange(0, 2 * np.pi * (1 + 2 / len(s[m][0])), 2 * np.pi / (len(s[m][0])))[0:len(s[m][0])]]
-    t[m] = [m for m in np.arange(0, 2 * np.pi*(1.0 + 1.0/100.0), 2 * np.pi / 99.0)]
+    t[m] = [m for m in np.arange(0, 2 * np.pi*(1.0 + 1.0/N), 2 * np.pi / (N-1.0))]
 
 # Average over all circles - pre-calculate parts of the sum of sines
 for m in range(0, M):
@@ -44,20 +45,21 @@ for m in range(0, M):
             s[m][n][index] += value  # add to front
             s[m][n][-(index + 1)] += value  # add to back, so beginning and end match and the circle stays closed
 
-# downsample so all circles have the same length (100 samples)
+# downsample so all circles have the same length (N samples)
 for m in range(0,M):
     r[m] = [[]]*5
     for n in range(0, 5):
-        r[m][n] = np.zeros(100)
-        for i in range(0, 99):
-            index = int(float(i)*(len(s[m][n])-1)/99.0)
+        r[m][n] = np.zeros(N)
+        for i in range(0, N-1):
+            index = int(float(i)*(len(s[m][n])-1)/(N-1.0))
             r[m][n][i] = s[m][n][index]
-        r[m][n][99] = s[m][n][0]    # close the circle
+        r[m][n][N-1] = s[m][n][0]    # close the circle
 
 first = True
 s = 0    # index into speed vector
 p = 0.0
 q = 1.0
+k = 0
 for f0 in np.arange(0, 1000, 0.01):
     c1 = np.square(np.sin(2*f0))
     c2 = np.square(np.sin(3*f0))
@@ -79,6 +81,9 @@ for f0 in np.arange(0, 1000, 0.01):
         ax.set_facecolor('k')
         fig.set_facecolor('k')
         fig.canvas.toolbar.pack_forget()
+        plt.subplots_adjust(top=1, bottom=0)
+        manager = plt.get_current_fig_manager()
+        manager.full_screen_toggle()
         plt.show(block=False)
 
     # plot curves
@@ -87,14 +92,14 @@ for f0 in np.arange(0, 1000, 0.01):
         #offset = np.sqrt((M-m))
         offset = M-m
         offset = np.sqrt(np.sqrt(offset*offset*offset))  # x^(3/4)
-        data = c1 * r[m][0] + c2 * r[m][1] + c3 * r[m][2] + c4 * r[m][3] + c5 * r[m][4] + 1
+        data = c1 * np.roll(r[m][0], k) + c2 * np.roll(r[m][1], 0) + c3 * np.roll(r[m][2], -k) + c4 * np.roll(r[m][3], 0) + c5 * np.roll(r[m][4], k) + 1
         data /= data.max()
         data *= offset
         data += offset
         if first:
             try:
                 if len(last_data):
-                    [lines[m]] = ax.plot(t[m], p*last_data + q*data, linewidth=thickness, color=(m/M, 0.5*m/M, 1-m/M))
+                    [lines[m]] = ax.plot(t[m], p*last_data + q*data, '.', linewidth=thickness, color=(m/M, 0.5*m/M, 1-m/M))
             except BaseException as e:
                 print("Plot exception {} for curve n={}".format(e, m))
         else:
@@ -102,9 +107,11 @@ for f0 in np.arange(0, 1000, 0.01):
                 if len(last_data):
                     lines[m].set_ydata(p*last_data + q*data)
                     if m == 1:
-                        lines[m].set_color((q*m/M, q*0.5*m/M, q*(1 - m/M)))
+                        lines[m].set_color((c1*q*m/M, c2*q*0.5*m/M, (1.0 - 0.5*c3)*q*(1 - m/M)))
                     elif m == M-1:
-                        lines[m].set_color((p*m/M, p*0.5*m/M, p*(1 - m/M)))
+                        lines[m].set_color((c1*p*m/M, c2*p*0.5*m/M, (1.0 - 0.5*c3)*p*(1 - m/M)))
+                    else:
+                        lines[m].set_color((c1*m/M, c2*0.5*m/M, (1.0 - 0.5*c3)*(1-m/M)))
             except BaseException as e:
                 print("Plot exception {} for curve n={}".format(e, m))
         last_data = data
@@ -115,3 +122,4 @@ for f0 in np.arange(0, 1000, 0.01):
         s = 0
     p = f[5][s] % 1
     q = 1.0 - p
+    k += 1
