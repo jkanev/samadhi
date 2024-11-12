58a38212303caed322a6a3518146d3c0c259b74c
Jacob Kanev <jkanev@zoho.com>
2024-11-12 19:48:14 +0100

    Finished first version of dancing dots tab
    The dancing dots are now in their own tab, also not displaying
    an extra figure any more. Opening and closing of both tabs works,
    however, when both the EEG display and the dancing dots are open,
    the update rate is not great, and the dots figure often flashes
    white.

--- samadhi.py
+++ samadhi.py
@@ -30,6 +30,7 @@ class Mind:
     # data streaming related
     _streaming = False   # whether we're streaming currently
     _showing_eegpsd = False     # whether we're showing the eeg/psd tab
+    _showing_ddots = False   # whether we're showing the dancing dots display
     _data_seconds = 10.0  # how much data do we have in the _eeg_data array
     _sampling_rate = 0   # sampling rate of eeg data
     _samples = 0         # seconds times sampling rate
@@ -92,6 +93,7 @@ class Mind:
     _ddots_layout = False
     _ddots_tab = False
     _ddots_canvas = False
+    _ddots_axes = False
 
     def __init__(self, combobox_streamname, lineedit_name, checkbox_connect, checkbox_analyse_psd,
                        checkbox_display_eegpsd, checkbox_visualisation_ddots,
@@ -165,10 +167,13 @@ class Mind:
         # set GUI elements
         self._checkbox_display_eegpsd.setEnabled(False)
         self._checkbox_display_eegpsd.setChecked(False)
+        self._checkbox_visualisation_ddots.setEnabled(False)
+        self._checkbox_visualisation_ddots.setChecked(False)
         self._lsl_label.setEnabled(False)
         self._checkbox_connect_lsl.setText("Click to connect")
         self._eeg_label.setEnabled(False)
         self._create_eegpsd_display_tab(False)
+        self._create_dancing_dots_display_tab(False)
 
     def _connect_eeg_stream(self, connect):
         """
@@ -235,7 +240,8 @@ class Mind:
             sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Maximum, QtWidgets.QSizePolicy.Policy.Maximum)
             self._eegpsd_tab.setSizePolicy(sizePolicy)
             self._parent_tabwidget.addTab(self._eegpsd_tab, "")
-            self._parent_tabwidget.setTabText(self._parent_tabwidget.indexOf(self._eegpsd_tab), self._name)
+            self._parent_tabwidget.setTabText(self._parent_tabwidget.indexOf(self._eegpsd_tab),
+                                              self._name + " -- EEG / Spectrum")
 
             # channel names
             c_names = ['C{}'.format(n+1) for n in range(0, self._channels)]
@@ -310,135 +316,157 @@ class Mind:
             sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Maximum, QtWidgets.QSizePolicy.Policy.Maximum)
             self._ddots_tab.setSizePolicy(sizePolicy)
             self._parent_tabwidget.addTab(self._ddots_tab, "")
-            self._parent_tabwidget.setTabText(self._parent_tabwidget.indexOf(self._ddots_tab), self._name)
+            self._parent_tabwidget.setTabText(self._parent_tabwidget.indexOf(self._ddots_tab),
+                                              self._name + " -- Dancing Dots")
 
-
-            # Create sum of sine waves of different frequencies
-            dt = 0.005
-            t0 = np.arange(0, 5 * np.pi, dt)
-            f = [(abs(np.sin(1 * t0))),
-                 (abs(np.sin(2 * t0))),
-                 (abs(np.sin(3 * t0))),
-                 (abs(np.sin(4 * t0))),
-                 (abs(np.sin(5 * t0))),
-                 ((np.sin(2 * t0) + 1) * 10.0)]
-
-            # Create 10 circles of different lengths
-            M = 40  # number of circles
-            N = 200  # points per circle
-            lines = [None] * M
+            # plt.plot(t[0], s[0][0])
             ylim = 32
-            thickness = 0.5
-            ax = None
-            fig = None
-            freq_start = 1.0
-            freq_step = 0.005
-            r = [[]] * M  # the interpolated circle data, zeros for now, consisting of M*N circles s[M][1..5], all the same length
-            s = [[]] * M  # the raw circle data, zeros for now, consisting of M*N circles s[M][1..5]
-            t = [[]] * M  # the base to plot against, will go from 0 to 2pi, t[M]
+            plt.ioff()
+            fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+            self._ddots_canvas = FigureCanvasQTAgg(fig)
+            self._ddots_axes = ax
+            self._ddots_layout.addWidget(self._ddots_canvas, 0, 0, 1, 1)
+            ax.grid(False)
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.set_theta_zero_location("N")
+            ax.set_ylim(0.0, ylim)
+            ax.set_facecolor('k')
+            fig.set_facecolor('k')
+
+            # start display thread
+            time.sleep(1)
+            self._showing_ddots = True
+            thdsp = threading.Thread(target=self._display_dancing_dots)
+            thdsp.start()
+
+        else:
+            if self._ddots_tab:
+                print("Removing Dancing Dots display tab.")
+                self._parent_tabwidget.removeTab(self._parent_tabwidget.indexOf(self._ddots_tab))
+            self._showing_ddots = False
+
+    def _display_dancing_dots(self):
+
+        # starting thread
+        print("Starting EEG/PSD display.")
+        while not len(self._eeg_data)\
+                or not len(self._fft_data)\
+                or not len(self._bnd_data)\
+                or not len(self._hst_data):
+            time.sleep(0.2)
+
+        # Create sum of sine waves of different frequencies
+        dt = 0.005
+        t0 = np.arange(0, 5 * np.pi, dt)
+        f = [(abs(np.sin(1 * t0))),
+             (abs(np.sin(2 * t0))),
+             (abs(np.sin(3 * t0))),
+             (abs(np.sin(4 * t0))),
+             (abs(np.sin(5 * t0))),
+             ((np.sin(2 * t0) + 1) * 10.0)]
+
+        # Create 10 circles of different lengths
+        M = 40  # number of circles
+        N = 200  # points per circle
+        lines = [None] * M
+        thickness = 0.5
+        ax = None
+        fig = None
+        freq_start = 1.0
+        freq_step = 0.005
+        r = [[]] * M  # the interpolated circle data, zeros for now, consisting of M*N circles s[M][1..5], all the same length
+        s = [[]] * M  # the raw circle data, zeros for now, consisting of M*N circles s[M][1..5]
+        t = [[]] * M  # the base to plot against, will go from 0 to 2pi, t[M]
+        for m in range(0, M):
+            s[m] = [[]] * 5
+            freq = freq_start + m * freq_step  # our circle frequency
+            print(freq)
+            for n in range(0, 5):
+                s[m][n] = np.zeros(int((2 / freq) * np.pi / dt))
+            # t[m] = [m for m in np.arange(0, 2 * np.pi * (1 + 2 / len(s[m][0])), 2 * np.pi / (len(s[m][0])))[0:len(s[m][0])]]
+            t[m] = [m for m in np.arange(0, 2 * np.pi * (1.0 + 1.0 / N), 2 * np.pi / (N - 1.0))]
+
+        # Average over all circles - pre-calculate parts of the sum of sines
+        for m in range(0, M):
+            for n in range(0, 5):
+                # only plot if you can fill the entire circle
+                for i in range(0, (len(f[n]) // len(s[m][n])) * len(s[m][n])):
+                    value = f[n][i] - 0.5
+                    index = i % len(s[m][n])
+                    s[m][n][index] += value  # add to front
+                    s[m][n][-(
+                                index + 1)] += value  # add to back, so beginning and end match and the circle stays closed
+
+        # downsample so all circles have the same length (N samples)
+        for m in range(0, M):
+            r[m] = [[]] * 5
+            for n in range(0, 5):
+                r[m][n] = np.zeros(N)
+                for i in range(0, N - 1):
+                    index = int(float(i) * (len(s[m][n]) - 1) / (N - 1.0))
+                    r[m][n][i] = s[m][n][index]
+                r[m][n][N - 1] = s[m][n][0]  # close the circle
+
+        first = True
+        s = 0  # index into speed vector
+        p = 0.0
+        q = 1.0
+        k = 0
+        while self._streaming and self._showing_ddots:
+
+            c1 = self._bnd_data[0]
+            c2 = self._bnd_data[1]
+            c3 = self._bnd_data[2]
+            c4 = self._bnd_data[3]
+            c5 = c1 - c2 + c3 - c4
+            print("Frequency bands: {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}".format(c1, c2, c3, c4, c5), end='\r')
+
+            # plot curves
+            last_data = []
             for m in range(0, M):
-                s[m] = [[]] * 5
-                freq = freq_start + m * freq_step  # our circle frequency
-                print(freq)
-                for n in range(0, 5):
-                    s[m][n] = np.zeros(int((2 / freq) * np.pi / dt))
-                # t[m] = [m for m in np.arange(0, 2 * np.pi * (1 + 2 / len(s[m][0])), 2 * np.pi / (len(s[m][0])))[0:len(s[m][0])]]
-                t[m] = [m for m in np.arange(0, 2 * np.pi * (1.0 + 1.0 / N), 2 * np.pi / (N - 1.0))]
-
-            # Average over all circles - pre-calculate parts of the sum of sines
-            for m in range(0, M):
-                for n in range(0, 5):
-                    # only plot if you can fill the entire circle
-                    for i in range(0, (len(f[n]) // len(s[m][n])) * len(s[m][n])):
-                        value = f[n][i] - 0.5
-                        index = i % len(s[m][n])
-                        s[m][n][index] += value  # add to front
-                        s[m][n][-(
-                                    index + 1)] += value  # add to back, so beginning and end match and the circle stays closed
-
-            # downsample so all circles have the same length (N samples)
-            for m in range(0, M):
-                r[m] = [[]] * 5
-                for n in range(0, 5):
-                    r[m][n] = np.zeros(N)
-                    for i in range(0, N - 1):
-                        index = int(float(i) * (len(s[m][n]) - 1) / (N - 1.0))
-                        r[m][n][i] = s[m][n][index]
-                    r[m][n][N - 1] = s[m][n][0]  # close the circle
-
-            first = True
-            s = 0  # index into speed vector
-            p = 0.0
-            q = 1.0
-            k = 0
-            for f0 in np.arange(0, 1000, 0.01):
-                c1 = self._bnd_data[0]
-                c2 = self._bnd_data[1]
-                c3 = self._bnd_data[2]
-                c4 = self._bnd_data[3]
-                c5 = c1 - c2 + c3 - c4
-                print("Frequency bands: {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}".format(c1, c2, c3, c4, c5), end='\r')
-
-                # initialise plot if we're here for first time
+                # offset = np.sqrt((M-m))
+                offset = M - m
+                offset = np.sqrt(np.sqrt(offset * offset * offset))  # x^(3/4)
+                data =   c1 * np.roll(r[m][0], k) \
+                       + c2 * np.roll(r[m][1], 0) \
+                       + c3 * np.roll(r[m][2],-k) \
+                       + c4 * np.roll(r[m][3], 0) \
+                       + c5 * np.roll(r[m][4], k) + 1
+                data /= data.max()
+                data *= offset
+                data += offset
                 if first:
-                    # plt.plot(t[0], s[0][0])
-                    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-                    ax.grid(False)
-                    ax.set_xticks([])
-                    ax.set_yticks([])
-                    ax.set_theta_zero_location("N")
-                    ax.set_ylim(0.0, ylim)
-                    ax.set_facecolor('k')
-                    fig.set_facecolor('k')
-                    self._ddots_canvas = FigureCanvasQTAgg(fig)
-                    self._ddots_layout.addWidget(self._ddots_canvas, 0, 0, 1, 1)
-
-                # plot curves
-                last_data = []
-                for m in range(0, M):
-                    # offset = np.sqrt((M-m))
-                    offset = M - m
-                    offset = np.sqrt(np.sqrt(offset * offset * offset))  # x^(3/4)
-                    data =   c1 * np.roll(r[m][0], k) \
-                           + c2 * np.roll(r[m][1], 0) \
-                           + c3 * np.roll(r[m][2],-k) \
-                           + c4 * np.roll(r[m][3], 0) \
-                           + c5 * np.roll(r[m][4], k) + 1
-                    data /= data.max()
-                    data *= offset
-                    data += offset
-                    if first:
-                        try:
-                            if len(last_data):
-                                [lines[m]] = ax.plot(t[m], p * last_data + q * data, '.', linewidth=thickness,
-                                                     color=(m / M, 0.5 * m / M, 1 - m / M))
-                        except BaseException as e:
-                            print("Plot exception {} for curve n={}".format(e, m))
-                    else:
-                        try:
-                            if len(last_data):
-                                lines[m].set_ydata(p * last_data + q * data)
-                                if m == 1:
-                                    lines[m].set_color(
-                                        (c1 * q * m / M, c2 * q * 0.5 * m / M, (1.0 - 0.5 * c3) * q * (1 - m / M)))
-                                elif m == M - 1:
-                                    lines[m].set_color(
-                                        (c1 * p * m / M, c2 * p * 0.5 * m / M, (1.0 - 0.5 * c3) * p * (1 - m / M)))
-                                else:
-                                    lines[m].set_color((c1 * m / M, c2 * 0.5 * m / M, (1.0 - 0.5 * c3) * (1 - m / M)))
-                        except BaseException as e:
-                            print("Plot exception {} for curve n={}".format(e, m))
-                    last_data = data
-                plt.pause(0.0001)
-                self._ddots_canvas.draw()
-                first = False
-                s += 1
-                if s > len(f[5]):
-                    s = 0
-                p = f[5][s] % 1
-                q = 1.0 - p
-                k += 1
-
+                    try:
+                        if len(last_data):
+                            [lines[m]] = self._ddots_axes.plot(t[m], p * last_data + q * data, '.', linewidth=thickness,
+                                                 color=(m / M, 0.5 * m / M, 1 - m / M))
+                    except BaseException as e:
+                        print("Plot exception {} for curve n={}".format(e, m))
+                else:
+                    try:
+                        if len(last_data):
+                            lines[m].set_ydata(p * last_data + q * data)
+                            if m == 1:
+                                lines[m].set_color(
+                                    (c1 * q * m / M, c2 * q * 0.5 * m / M, (1.0 - 0.5 * c3) * q * (1 - m / M)))
+                            elif m == M - 1:
+                                lines[m].set_color(
+                                    (c1 * p * m / M, c2 * p * 0.5 * m / M, (1.0 - 0.5 * c3) * p * (1 - m / M)))
+                            else:
+                                lines[m].set_color((c1 * m / M, c2 * 0.5 * m / M, (1.0 - 0.5 * c3) * (1 - m / M)))
+                    except BaseException as e:
+                        print("Plot exception {} for curve n={}".format(e, m))
+                last_data = data
+            self._ddots_canvas.draw()
+            first = False
+            time.sleep(0.01)
+            s += 1
+            if s > len(f[5]):
+                s = 0
+            p = f[5][s] % 1
+            q = 1.0 - p
+            k += 1
 
     def _display_eeg_psd(self):
 
