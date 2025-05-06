2381368c3897f1353ff9ecc5b62f0e41bfa4a999
Jacob <jkanev@ant-neuro.com>
2025-05-06 18:55:40 +0200

    Fixed problems with windows-installer built packages
    When built with pyinstaller for windows, there's a problem with the
    stderr not being present, hence the fault handler needs to be removed.

--- samadhi/__main__.py
+++ samadhi/__main__.py
@@ -4,8 +4,8 @@ import sys
 
 # main starting file for use with or with pyinstaller for creating executable binaries.
 # For normal running please execute the software as a module (python3 -m samadhi);
-import faulthandler
+#import faulthandler
 from samadhi import Samadhi
 
-faulthandler.enable()
+#faulthandler.enable()
 Samadhi()
